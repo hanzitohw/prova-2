d3fff7f630503696536d1605aa303b4ef24cd912
hans wahrlich <hanshwkfuri@gmail.com>
2024-11-25 15:26:11 -0300

    Update Berserk.js

--- Berserk.js
+++ Berserk.js
@@ -4,7 +4,6 @@ themeToggleButton.addEventListener('click', function() {
   document.body.classList.toggle('dark-theme');
 });
 
-// Exibir pop-up após o envio do formulário
 const form = document.getElementById('contact-form');
 const successPopup = document.getElementById('success-popup');
 
@@ -13,22 +12,19 @@ form.addEventListener('submit', function(event) {
   
   // Mostrar pop-up
   successPopup.style.display = 'flex';
-  form.reset();  // Limpar o formulário após envio
+  form.reset();
 });
 
-// Fechar o pop-up
 function closePopup() {
   successPopup.style.display = 'none';
 }
 
-// Validação do formulário de contato
 constform = document.getElementById('contact-form');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   document.getElementById('success-message').style.display = 'block';
 });
 
-// Adicionar funcionalidade suave para rolagem
 const menuLinks = document.querySelectorAll('.menu a');
 menuLinks.forEach(link => {
   link.addEventListener('click', (e) => {
